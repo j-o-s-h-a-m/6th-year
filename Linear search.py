ddf42ee0864e2384c3d526519b6e52aba5ecd955
JOSHAM <22SJoshi.acc@lwetb.ie>
2026-09-02 23:04:11 +0100

    Update Linear search.py

--- Linear search.py
+++ Linear search.py
@@ -1,1 +1,14 @@
+#Author - Saksham Joshi
+#Date - 02 september 2026
 
+lst = []
+
+a = 0
+while a == 0 :
+    list1 = list(input("Enter element for your list: "))
+    lst = lst + list1
+    if list1 == [] :
+        break
+x = input("enter the element you want to find in the list: ")
+index = lst.index(x)
+print(index)
