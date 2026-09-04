229f2fad7577466965ac03e5bf70dbe4513962d7
JOSHAM <22SJoshi.acc@lwetb.ie>
2026-09-04 10:45:18 +0100

    Create bubble sort.py

---
+++ bubble sort.py
@@ -0,0 +1,8 @@
+#author saksham joshi date 04 september 2026
+lst = [10 , 8 , 6 , 4 , 2]
+count = 0
+for i in range(0,len(lst)):
+    count += 1
+    if lst[i] > lst[i+1]:
+        list.insert(list.index[list[count]],lst.index[list[i]])
+        print(lst)
