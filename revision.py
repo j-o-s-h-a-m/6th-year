4c7e3582f1afd904a16a226e076b2ebe5d29ecf5
JOSHAM <22SJoshi.acc@lwetb.ie>
2026-09-24 10:46:24 +0100

    Create revision.py

---
+++ revision.py
@@ -0,0 +1,14 @@
+def hola (mean):
+    count = 0
+    total = 0
+    for i in mean_inp:
+        count+1
+        i = int(i)
+        total += i
+    average = mean//count
+        
+        
+    
+    
+mean_inp = input('Enter A list : ')
+hola(mean_inp)
