f491365e7d8db185597cf154c81dd1432b9448a8
JOSHAM <22SJoshi.acc@lwetb.ie>
2026-09-25 10:40:38 +0100

    Update revision.py

--- revision.py
+++ revision.py
@@ -1,14 +1,24 @@
 def hola (mean):
     count = 0
     total = 0
-    for i in mean_inp:
-        count+1
+    for i in lst:
+        count += 1
         i = int(i)
         total += i
-    average = mean//count
+    average = total//count
+    return average
         
         
     
+stop = 0
+lst = []
+while stop == 0:
+    lst_in = input('Enter the number u want to add into a list(Press enter to end input: ')
+    if lst_in != '':
+        lst.append(lst_in)
+    else:
+        break
     
-mean_inp = input('Enter A list : ')
-hola(mean_inp)
+
+average1 = hola(lst)
+print(average1)
